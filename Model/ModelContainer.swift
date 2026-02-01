5821f2ca01e99098a8f55436e86017e3b93e60c7
Henry Yang <gyeongho.yang@deliveryhero.com>
2026-02-01 01:09:47 +0100

    apply app group

--- Model/ModelContainer.swift
+++ Model/ModelContainer.swift
@@ -7,13 +7,10 @@ extension ModelContainer {
       Account.self,
       Note.self,
     ])
-    let appSupportURL = FileManager.default.urls(
-      for: .applicationSupportDirectory, in: .userDomainMask
-    ).first!
-    let dbFolderURL = appSupportURL.appendingPathComponent("kr.ygh.keep")
-    try? FileManager.default.createDirectory(at: dbFolderURL, withIntermediateDirectories: true)
-    let url = dbFolderURL.appendingPathComponent("default.sqlite")
-    print(url)
+    let appGroupURL = FileManager.default.containerURL(
+      forSecurityApplicationGroupIdentifier: "group.kr.ygh.keep")!
+    try? FileManager.default.createDirectory(at: appGroupURL, withIntermediateDirectories: true)
+    let url = appGroupURL.appendingPathComponent("default.sqlite")
     let modelConfiguration = ModelConfiguration(schema: schema, url: url)
 
     do {
